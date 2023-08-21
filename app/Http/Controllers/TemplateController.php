3b15d0db919f809a5f04d886fdb3a3b495c82dcf
Lê Đức Mạnh <manhld@tienngay.vn>
2023-08-21 19:06:40 +0700

    first commit

---
+++ app/Http/Controllers/TemplateController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class TemplateController extends BaseController
+{
+    public function example()
+    {
+        return view('template.example');
+    }
+
+
+}
