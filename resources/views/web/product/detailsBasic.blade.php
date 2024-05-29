41a73cf9b997c72740708e362ad4a8473876b0e8
Quanabcd <leequan1102@gmail.com>
2024-05-29 19:15:23 +0700

    Update detailsBasic.blade.php

--- resources/views/web/product/detailsBasic.blade.php
+++ resources/views/web/product/detailsBasic.blade.php
@@ -317,7 +317,7 @@
                         </div>
                         <div class="tabBatteryPackages" id="tabBatteryPackages" role="tabpanel"
                             aria-labelledby="battery-tab">
-                            <div class="breaking-battery-tab d-flex justify-content-between" style="display: block;">
+                            <div class="tab-pane-info breaking-battery-tab d-flex justify-content-between" style="display: block;">
                                
                                     {!! $product->chassis !!}
                                
@@ -325,7 +325,7 @@
                         </div>
                         <div class="tabdongcoPackages" id="tabdongcoPackages" role="tabpanel"
                             aria-labelledby="battery-tab" style="display: block;">
-                            <div class="breaking-battery-tab d-flex justify-content-between">
+                            <div class="tab-pane-info breaking-battery-tab d-flex justify-content-between">
                               
                                     {!! $product->engine !!}
                               
