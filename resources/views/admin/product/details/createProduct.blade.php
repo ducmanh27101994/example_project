250c9189f75e3486dad38fdc59c6827d7f1da92d
Quanabcd <leequan1102@gmail.com>
2024-05-29 14:18:41 +0700

    udate upload image

--- resources/views/admin/product/details/createProduct.blade.php
+++ resources/views/admin/product/details/createProduct.blade.php
@@ -183,7 +183,8 @@
 
                                                 <div class="block_sp">
                                                     <div class="form-group">
-                                                        <label class="text-no-bold" data-bind="text: Title">Hình ảnh block chi tiết</label>
+                                                        <label class="text-no-bold" data-bind="text: Title">Hình ảnh
+                                                            block chi tiết</label>
                                                         <input type="file" name="block_detail_image" id="flupload"
                                                             class="btn-change-link pull-right" style="width:180px;">
                                                     </div>
@@ -372,10 +373,12 @@
                                 <div class="gallarey">
                                     <label for="album">Ảnh chi tiết xe(up nhiều ảnh một lúc, 494 × 636 px)</label>
 
-                                    <input name="vehicle_detail_photos_title" type="text" value="" id="vehicle_detail_photos_title"
-                                        class="inputbox" placeholder="" style="width:100%;">
-                                    <textarea name="vehicle_detail_photos_desc" rows="5" cols="70" id="vehicle_detail_photos_desc"
-                                        class="inputbox" style="width: 100%; height:80px;"></textarea>
+                                    <input name="vehicle_detail_photos_title" type="text" value=""
+                                        id="vehicle_detail_photos_title" class="inputbox" placeholder=""
+                                        style="width:100%;">
+                                    <textarea name="vehicle_detail_photos_desc" rows="5" cols="70"
+                                        id="vehicle_detail_photos_desc" class="inputbox"
+                                        style="width: 100%; height:80px;"></textarea>
 
                                     <div id="uploadfiles" maxcount="16" input="ctl33_ctl00_FileServer2_ctl00"
                                         class="uploadfdfile upload-create-form">
@@ -431,6 +434,160 @@
                                     <div id="image-preview-2"></div>
                                 </div>
 
+                                <div class="upload__box">
+                                    <div class="upload__btn-box">
+                                        <label class="upload__btn">
+                                            <p>Upload images</p>
+                                            <input type="file" multiple="" data-max_length="20"
+                                                class="upload__inputfile">
+                                        </label>
+                                    </div>
+                                    <div class="upload__img-wrap"></div>
+                                    <style>
+                                        .upload__box {
+                                            padding: 40px;
+                                        }
+
+                                        .upload__inputfile {
+                                            width: 0.1px;
+                                            height: 0.1px;
+                                            opacity: 0;
+                                            overflow: hidden;
+                                            position: absolute;
+                                            z-index: -1;
+                                        }
+
+                                        .upload__btn {
+                                            display: inline-block;
+                                            font-weight: 600;
+                                            color: #fff;
+                                            text-align: center;
+                                            min-width: 116px;
+                                            padding: 5px;
+                                            transition: all 0.3s ease;
+                                            cursor: pointer;
+                                            border: 2px solid;
+                                            background-color: #4045ba;
+                                            border-color: #4045ba;
+                                            border-radius: 10px;
+                                            line-height: 26px;
+                                            font-size: 14px;
+                                        }
+
+                                        .upload__btn:hover {
+                                            background-color: unset;
+                                            color: #4045ba;
+                                            transition: all 0.3s ease;
+                                        }
+
+                                        .upload__btn-box {
+                                            margin-bottom: 10px;
+                                        }
+
+                                        .upload__img-wrap {
+                                            display: flex;
+                                            flex-wrap: wrap;
+                                            margin: 0 -10px;
+                                        }
+
+                                        .upload__img-box {
+                                            width: 200px;
+                                            padding: 0 10px;
+                                            margin-bottom: 12px;
+                                        }
+
+                                        .upload__img-close {
+                                            width: 24px;
+                                            height: 24px;
+                                            border-radius: 50%;
+                                            background-color: rgba(0, 0, 0, 0.5);
+                                            position: absolute;
+                                            top: 10px;
+                                            right: 10px;
+                                            text-align: center;
+                                            line-height: 24px;
+                                            z-index: 1;
+                                            cursor: pointer;
+                                        }
+
+                                        .upload__img-close:after {
+                                            content: "✖";
+                                            font-size: 14px;
+                                            color: white;
+                                        }
+
+                                        .img-bg {
+                                            background-repeat: no-repeat;
+                                            background-position: center;
+                                            background-size: cover;
+                                            position: relative;
+                                            padding-bottom: 100%;
+                                        }
+                                    </style>
+                                    <script>
+                                        jQuery(document).ready(function () {
+                                            ImgUpload();
+                                        });
+
+                                        function ImgUpload() {
+                                            var imgWrap = "";
+                                            var imgArray = [];
+
+                                            $('.upload__inputfile').each(function () {
+                                                $(this).on('change', function (e) {
+                                                    imgWrap = $(this).closest('.upload__box').find('.upload__img-wrap');
+                                                    var maxLength = $(this).attr('data-max_length');
+
+                                                    var files = e.target.files;
+                                                    var filesArr = Array.prototype.slice.call(files);
+                                                    var iterator = 0;
+                                                    filesArr.forEach(function (f, index) {
+
+                                                        if (!f.type.match('image.*')) {
+                                                            return;
+                                                        }
+
+                                                        if (imgArray.length > maxLength) {
+                                                            return false
+                                                        } else {
+                                                            var len = 0;
+                                                            for (var i = 0; i < imgArray.length; i++) {
+                                                                if (imgArray[i] !== undefined) {
+                                                                    len++;
+                                                                }
+                                                            }
+                                                            if (len > maxLength) {
+                                                                return false;
+                                                            } else {
+                                                                imgArray.push(f);
+                                                                console.log(imgArray)
+                                                                var reader = new FileReader();
+                                                                reader.onload = function (e) {
+                                                                    var html = "<div class='upload__img-box'><div style='background-image: url(" + e.target.result + ")' data-number='" + $(".upload__img-close").length + "' data-file='" + f.name + "' class='img-bg'><div class='upload__img-close'></div></div></div>";
+                                                                    imgWrap.append(html);
+                                                                    iterator++;
+                                                                }
+                                                                reader.readAsDataURL(f);
+                                                            }
+                                                        }
+                                                    });
+                                                });
+                                            });
+
+                                            $('body').on('click', ".upload__img-close", function (e) {
+                                                var file = $(this).parent().data("file");
+                                                for (var i = 0; i < imgArray.length; i++) {
+                                                    if (imgArray[i].name === file) {
+                                                        imgArray.splice(i, 1);
+                                                        console.log(imgArray)
+                                                        break;
+                                                    }
+                                                }
+                                                $(this).parent().parent().remove();
+                                            });
+                                        }
+                                    </script>
+                                </div>
 
                             </div>
 
